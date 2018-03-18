226fcc00568b3807db76d6390778b985a50e0be9
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-03-18 13:44:08 -0600

    fixing remote server location

--- routes/meals.js
+++ routes/meals.js
@@ -10,4 +10,30 @@ router.get("/", (request, response, next) => {
     .catch(next)
 })
 
+router.get("/:id", (request, response, next) => {
+    queries.read(request.params.id).then(meals => {
+        meals
+            ? response.json({coffee})
+            : response.status(404).json({message: 'Not found'})
+    }).catch(next);
+});
+
+router.post("/", (request, response, next) => {
+    queries.create(request.body).then(meals => {
+        response.status(201).json({meals});
+    }).catch(next);
+});
+
+router.delete("/:id", (request, response, next) => {
+    queries.delete(request.params.id).then(() => {
+        response.status(204).json({deleted: true});
+    }).catch(next);
+});
+
+router.put("/:id", (request, response, next) => {
+    queries.update(request.params.id, request.body).then(meals => {
+        response.json({meals});
+    }).catch(next);
+});
+
 module.exports = router
